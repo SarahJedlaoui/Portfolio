0e7440a8bbecdfeac26bc11fe37fa96a0a3f1959
Sarah Jedloaui <sarajedlaoui999@gmail.com>
2022-11-09 19:54:51 +0100

    first committ

--- src/containers/greeting/FeelingProud.js
+++ src/containers/greeting/FeelingProud.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./FeelingProud.css";
-import img from "../../assests/images/software-developer-germany.svg"
+import img from "../../assests/images/software-developer-germany.svg";
+
 function FeelingProud(props) {
   
   return (
-    <img src={img} alt="" style={{height:"350px",marginLeft:"100px"}}/>
+    <img src={img} alt="" style={{height:"350px",marginLeft:"100px"}}/> 
   );
 }
 
