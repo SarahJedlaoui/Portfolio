4947382118b89071540a6710ed940c81121093f6
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-12-16 12:41:45 +0100

    updates

--- src/containers/skills/FullStackImg.js
+++ src/containers/skills/FullStackImg.js
@@ -2,6 +2,5 @@ import React from "react";
 import img from "../../assests/images/code3.svg";
 import "./full.css";
 export default function FullStackImg(props) {
-  const theme = props.theme;
   return <img src={img} alt="Feeling Proud" className="feeling-proud-img" />;
 }
